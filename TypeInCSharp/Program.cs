4098d2a0c41dd2c683f295b8a87f6128e8c9856e
Gino <ggino3567@gmail.com>
2025-07-20 21:47:48 +0800

    Update: 修改及新增 - 新增某些註解 - 修改Array的代碼 [2025/7/20]

--- TypeInCSharp/Program.cs
+++ TypeInCSharp/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace TypeInCSharp
+﻿using System.Numerics;
+
+namespace TypeInCSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //容易搞錯:
+            //static靜態方法: 不需實例化可直接調用、一運作就存在
+            Student.GetStudent(); //這就是靜態方法
+            Student student = new Student();
+            student.GetStudentWithoutStatic(); //這就是非靜態方法
+
             /*
              * C#類型系統
              *                         -> class
@@ -17,6 +25,7 @@
 
             //方法中宣告的區域變數有明確賦值原則 不能寫作int MyNumber; 會報錯
             //成員變數未賦值將給預設值如0, null
+            
             /*
              * C# 的 Stack (Runtime Stack)與JS相同後進先出(他媽的這是廢話):
              * 儲存執行上下文： 這個框架包含了該函數執行所需的所有資訊，例如：
@@ -27,40 +36,61 @@
              * C# Stack： 主要用於儲存實值型別、方法參數和位址。
              * 參考型別 (Reference Types) 的實際資料則儲存在 Heap (堆積) 上，Stack 裡只儲存對 Heap 上資料的參考位址。
              */
+
             //null == empty
 
-            //class
-            Intro Intro = new Intro();
-            Intro.SayHello();
-            Intro.Name = "Gino";
-            Console.WriteLine(Intro.Name);
+            /*
+             * 宣告方法時，public void MethodName(Parameter形式參數)
+             * 調用方法時，Object.Class.MethodName(Argument實際參數)
+             */
 
-            //Array
-            int[] NewArray = new int[10]; //Array.length=10, [0, 0, 0, ...n+1]
-            NewArray[0] = 10; //[10, 0, 0, ...n+1]
+            //Array & Array List<T> & Dictionary<TKey, TValue>
+            //<T>等於參數類型
+            int[] intArray = new int[10]; //Array在宣告時就把長度限制住了 長度為10
+            string[] strArray = new string[] {"T1", "T2"}; //無法改變 長度為2
+            List<string> foodSets = new List<string>
+            {
+                "炸雞",
+                "炒麵",
+                "烤串"
+            };
+            System.Console.WriteLine($"FoodSets[0]: {foodSets[0]}"); //炸雞
+            System.Console.WriteLine($"FoodSets: {foodSets.Count}"); //3
 
-            int[] StaticArray = { 10, 20, 30 };
-            Console.WriteLine(StaticArray.Length);
+            List<string> newFoodSets = new List<string>(foodSets); //建構子直接初始化一個副本
+            //NewFoodSets = {...FoodSets}; c#不支援展開運算符
+            newFoodSets.Add("大便");
+            System.Console.WriteLine($"NewFoodSets: {newFoodSets.Count}");
 
-            //Struct(指結構體 不是struct關鍵字)
+            Dictionary<int, string> students = new Dictionary<int, string>
+            {
+                {1, "這是1" },
+                {2, "這是2" },
+                {3, "這是3" },
+            };
+            System.Console.WriteLine($"Students[2]: {students[2]}"); //這是2
+
+
+            Dictionary<int, string> employees = new Dictionary<int, string> ();
+            employees.Add(1, "Hello");
+
+            //常數 & 變數 & 超大杯
             const double pi = 3.14159; //常數不可變
             int @int = 10; //32bit 佔據4個位元組 宣告變數後在記憶體中尋找空的位址存放 無論數值是否定義
-            long @long = 20L;
+            long @long = 20L; //64bit 真大
             sbyte sb = 100; //8bit, 011....00
             ushort st = 1000; //16bit, ("0符號位"0000011(高位)11101000(低位))
             short s = -1000; //16bit, (按位取反再加一(加一:進位，若為1則進位遇到0轉一停止進位) => "1符號位"1111100(高位)00011000(低位))
             string str = Convert.ToString(s, 2);
-            Console.WriteLine(str); //1111110000011000
+            //Console.WriteLine(str); //1111110000011000
+            BigInteger result = BigInteger.One; //超大杯 無限記憶體空間
 
-            //Boxing & Unboxing(損失性能 貌似少用?)
+            //裝箱 & 拆箱(損失性能 貌似少用?)
             int SampleBox = 100; //根據int切4位元組 內容為100
             object obj; //引用類型先切4位元組出來 預設值為0
             obj = SampleBox; //裝箱: 此時SampleBox中的內容將被丟到Heap裡 並將該位址存入obj引用
             int SampleUnbox = (int)obj; //拆箱: 將obj引用的Heap中的內容複製進SampleUnbox裏頭
-            Console.WriteLine(SampleUnbox);
-
-            //enum
-            //{...}
+            //Console.WriteLine(SampleUnbox);
 
             //class : reference type details
             Student stu; //根據Student這個類別在ram中切一塊32bit的大小擺放 內容為0 => 記憶體位址: 10000010
@@ -77,26 +107,15 @@
     {
         uint StudentId;
         ushort Score;
-    }
 
-    class Intro
-    {
-        public static int Amount;
-        public int Age;
-        public string Name;
-
-        //Method
-        public void SayHello()
+        public static void GetStudent()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("這是靜態方法");
         }
 
-        //enum
-        public enum WindowState
+        public void GetStudentWithoutStatic()
         {
-            Nor = 0,
-            Min = 1,
-            Max = 2,
+            Console.WriteLine("這不是靜態方法");
         }
     }
 }
